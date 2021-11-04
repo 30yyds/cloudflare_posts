0e3800c3c6758225753b5eef78ac891ecaadfaa7
Kyle Yang <chang.yang@verizonmedia.com>
2021-11-04 00:55:20 -0700

    fix bug

--- src/components/form.js
+++ src/components/form.js
@@ -8,28 +8,28 @@ export default class FormSubmission extends React.Component {
       <div>
         <form action="https://serverless-api.30yyds.workers.dev/api/create-posts" method="POST">
           <div>
-            <label htmlFor="title" className="block text-sm font-medium text-warm-gray-900">
+            <label htmlFor="title">
               <h2><center>Title</center></h2>
             </label>
-            <div>
-              <input type="text" name="title" />
+            <div style="width:800px; margin:0 auto;">
+              <input type="text" name="title" id="title" />
             </div>
           </div>
           <div>
-            <label htmlFor="username" className="block text-sm font-medium text-warm-gray-900">
+            <label htmlFor="username">
               <h2><center>Username</center></h2>
             </label>
-            <div>
-              <input type="text" name="username" />
+            <div style="width:800px; margin:0 auto;">
+              <input type="text" name="username" id="username" />
             </div>
           </div>
           <div className="sm:col-span-10">
             <div className="flex justify-between">
-              <label htmlFor="content" className="block text-sm font-medium text-warm-gray-900">
+              <label htmlFor="content">
                 <h2><center>Post Content</center></h2>
               </label>
             </div>
-            <div className="mt-1">
+            <div className="mt-1" style="width:800px; margin:0 auto;">
               <textarea
                 id="content"
                 name="content"
@@ -42,7 +42,7 @@ export default class FormSubmission extends React.Component {
             </div>
           </div>
           <button type="submit">
-            Submit
+            Create Post
           </button>
         </form>
         <p>
