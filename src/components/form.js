66ceb70f321c429f0825cd9b642ad5b06b0f54b4
Kyle Yang <chang.yang@verizonmedia.com>
2021-11-04 22:24:56 -0700

    update form

--- src/components/form.js
+++ src/components/form.js
@@ -2,35 +2,52 @@ import React from 'react';
 import { Link } from "@reach/router";
 
 export default class FormSubmission extends React.Component {
+  
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const url = 'https://serverless-api.30yyds.workers.dev/api/create-posts'
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title, username, content })
+    };
+    fetch(url, requestOptions)
+        .then(response => console.log('Created post successfully'))
+        .catch(error => console.log('Form submit error', error))
+  };
 
   render() {
     return (
       <div>
-        <form action="https://serverless-api.30yyds.workers.dev/api/create-posts" method="POST">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="title" className="block text-sm font-medium text-warm-gray-900">
             <h2><center>Title</center></h2>
           </label>
-          <input class="center-block" type="text" name="title" id="title" />
+          <input border="solid 1px black" type="text" name="title" id="title" />
           <label htmlFor="username" className="block text-sm font-medium text-warm-gray-900">
             <h2><center>Username</center></h2>
           </label>
-          <input class="center-block" type="text" name="username" id="username" />
+          <input border="solid 1px black" type="text" name="username" id="username" />
           <label htmlFor="content" className="block text-sm font-medium text-warm-gray-900">
             <h2><center>Post Content</center></h2>
           </label>
           <textarea
             id="content"
             name="content"
-            rows="10"
+            rows="20" cols="100"
+            border="solid 1px black"
             text-align="justify"
             text-align-last="center"
             placeholder="Please write something here."
             required
             defaultValue={''}
           />
-          <button type="submit">Create Your Post</button>
+          <div style="width:800px; margin:0 auto;">
+            <button type="submit">Create Your Post</button>
+          </div>  
           <div>
-            <h2><center><Link to={`/create-post`}>Add New Post Here</Link></center></h2> 
+            <h2><center><Link to={`/`}>Go Back</Link></center></h2> 
           </div>
         </form>
       </div>
