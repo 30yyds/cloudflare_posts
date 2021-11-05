63353b7bf0887d085570aa2d073f4dc0d86cd442
Kyle Yang <chang.yang@verizonmedia.com>
2021-11-04 21:55:49 -0700

    update

--- src/components/form.js
+++ src/components/form.js
@@ -10,24 +10,31 @@ export default class FormSubmission extends React.Component {
           <label htmlFor="title" className="block text-sm font-medium text-warm-gray-900">
             <h2><center>Title</center></h2>
           </label>
-          <input type="text" name="title" id="title" />
+          <input class="center-block" type="text" name="title" id="title" />
           <label htmlFor="username" className="block text-sm font-medium text-warm-gray-900">
             <h2><center>Username</center></h2>
           </label>
-          <input type="text" name="username" id="username" />
+          <input class="center-block" type="text" name="username" id="username" />
           <label htmlFor="content" className="block text-sm font-medium text-warm-gray-900">
             <h2><center>Post Content</center></h2>
           </label>
           <textarea
             id="content"
             name="content"
-            aria-describedby="content-max"
+            rows="10"
+            text-align: justify;
+            text-align-last: center;
             placeholder="Please write something here."
             required
             defaultValue={''}
           />
           <button type="submit">Create Your Post</button>
         </form>
+        <div style="margin-top:10px">
+          <p>
+            <Link to="/">Go back</Link>
+          </p> 
+        </div>
       </div>
     );
   }
