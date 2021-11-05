2195e0d50bdd18d001cf76bb3524779c2e11ff43
Kyle Yang <chang.yang@verizonmedia.com>
2021-11-05 00:21:08 -0700

    update form

--- src/components/form.js
+++ src/components/form.js
@@ -10,6 +10,7 @@ export default class FormSubmission extends React.Component {
     event.preventDefault();
 
     const url = 'https://serverless-api.30yyds.workers.dev/api/create-posts';
+    
     const data = new FormData(event.target);
     const value = Object.fromEntries(data.entries());
 
