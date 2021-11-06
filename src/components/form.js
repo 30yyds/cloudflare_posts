f2ee3a4ceee6645035ac1d9edf12bcb0b3df03ab
Kyle Yang <chang.yang@verizonmedia.com>
2021-11-06 01:00:28 -0700

    test around

--- src/components/form.js
+++ src/components/form.js
@@ -7,6 +7,20 @@ export default class FormSubmission extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+  async function gatherResponse(response) {
+    const { headers } = response;
+    const contentType = headers.get("content-type") || "";
+    if (contentType.includes("application/json")) {
+      return JSON.stringify(await response.json());
+    } else if (contentType.includes("application/text")) {
+      return response.text();
+    } else if (contentType.includes("text/html")) {
+      return response.text();
+    } else {
+      return response.text();
+    }
+  }
+
   async handleSubmit(event) {
     event.preventDefault();
     
